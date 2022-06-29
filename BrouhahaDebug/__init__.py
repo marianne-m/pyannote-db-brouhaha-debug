d10918153866aa17a5f0494af85e3c8449215751
Marianne <marianne.metais@grenoble-inp.org>
2022-06-29 12:01:27 +0200

    add target_features (snr & c50) to Regression protocol

--- BrouhahaDebug/__init__.py
+++ BrouhahaDebug/__init__.py
@@ -30,29 +30,48 @@ __version__ = get_versions()['version']
 del get_versions
 
 
-from pyannote.core import Annotation, Timeline
+from pyannote.core import Annotation, Timeline, SlidingWindow, SlidingWindowFeature
 from pyannote.database import Database
 from pyannote.database.protocol import SpeakerDiarizationProtocol
 from pathlib import Path
 
+import numpy as np
+import pandas as pd
+
 
 class RegressionPoetryRecital(SpeakerDiarizationProtocol):
 
+    SNR_SLIDING_WINDOW = SlidingWindow(duration=2.0, step=0.01, start=0)
     ANNOT_FOLDER = Path(__file__).parent / Path("data/annotations")
+    SNR_FOLDER = Path(__file__).parent / Path("data/snr")
+    C50_FOLDER = Path(__file__).parent / Path("data/reverb")
 
     def _subset_iter(self, subset):
 
         annot_folder = self.ANNOT_FOLDER / Path(subset)
+        c50_values = pd.read_csv(
+            self.C50_FOLDER / Path(f"{subset}_reverb_labels.txt"),
+            sep=" ",
+            names=["filename", "c50"]
+        )
         for annot_file in annot_folder.glob("*.json"):
             with open(annot_file) as json_file:
                 json_data = json.load(json_file)
             annotation = Annotation.from_json(json_data)
             annotated = Timeline([annotation.get_timeline().extent()])
+            snr_array = np.load(self.SNR_FOLDER / f"{annot_file.stem}.npy")
+            snr_array = np.expand_dims(snr_array, axis=1)
+            snr_feat = SlidingWindowFeature(snr_array, sliding_window=self.SNR_SLIDING_WINDOW)
+            c50 = c50_values.c50[c50_values.filename == annot_file.stem]
             current_file = {
                 'database': 'BrouhahaDebug',
                 'uri': annotation.uri,
                 'annotated': annotated,
                 'annotation': annotation,
+                'target_features': {
+                    "c50": c50,
+                    "snr": snr_feat
+                }
             }
 
 
